28215d4ce60e831b1b7391161b1fdeaa8946a410
Michael Maclean <michael@no-surprises.co.uk>
2011-06-04 20:18:10 +0100

    Move callbacks to a separate file

--- mapviewer.c
+++ mapviewer.c
@@ -30,6 +30,7 @@
 #include "aprsis.h"
 #include "station.h"
 #include "mapviewer.h"
+#include "callbacks.h"
 
 OsmGpsMap *map;
 GtkEntry *latent;
@@ -75,114 +76,6 @@ static GOptionEntry entries[] =
 	{ "packet-log-file", 'l', 0, G_OPTION_ARG_FILENAME, &packet_log_file, "Log network IO to a file", "FILE" },
   { NULL }
 };
-
-
-
-static gboolean
-on_button_press_event (GtkWidget *widget, GdkEventButton *event, gpointer user_data)
-{
-    OsmGpsMapPoint coord;
-    float lat, lon;
-    OsmGpsMap *map = OSM_GPS_MAP(widget);
-    return FALSE;
-}
-
-static gboolean
-on_button_release_event (GtkWidget *widget, GdkEventButton *event, gpointer user_data)
-{
-    float lat,lon;
-    OsmGpsMap *map = OSM_GPS_MAP(widget);
-
-    g_object_get(map, "latitude", &lat, "longitude", &lon, NULL);
-    gchar *msg = g_strdup_printf("%f,%f",lat,lon);
-    g_free(msg);
-
-    return FALSE;
-}
-
-G_MODULE_EXPORT gboolean
-on_zoom_in_clicked_event (GtkWidget *widget, gpointer user_data)
-{
-    int zoom;
-    OsmGpsMap *map = OSM_GPS_MAP(user_data);
-    g_object_get(map, "zoom", &zoom, NULL);
-    osm_gps_map_set_zoom(map, zoom+1);
-    return FALSE;
-}
-
-static gboolean
-on_zoom_out_clicked_event (GtkWidget *widget, gpointer user_data)
-{
-    int zoom;
-    OsmGpsMap *map = OSM_GPS_MAP(user_data);
-    g_object_get(map, "zoom", &zoom, NULL);
-    osm_gps_map_set_zoom(map, zoom-1);
-    return FALSE;
-}
-
-static gboolean
-on_home_clicked_event (GtkWidget *widget, gpointer user_data)
-{
-    OsmGpsMap *map = OSM_GPS_MAP(user_data);
-	//change this bitch up
-    osm_gps_map_set_center_and_zoom(map,55.00,-4.0,5);
-    return FALSE;
-}
-G_MODULE_EXPORT void
-on_about_clicked_event (GtkWidget *widget, gpointer user_data)
-{
-	gtk_dialog_run (GTK_DIALOG(about) );
-	gtk_widget_hide (GTK_WIDGET(about));
-	//return FALSE;
-}
-G_MODULE_EXPORT gpointer
-on_properties_clicked_event (GtkWidget *widget, gpointer user_data)
-{    
-	gtk_window_present( GTK_WINDOW( popup ) );
-	return FALSE;
-}
-static gboolean
-on_properties_ok_clicked (GtkWidget *widget, aprs_details *properties)
-{
-	double oldlat = properties->lat;
-	double oldlon = properties->lon;   
-	printf("We were: %f%f\n",oldlat,oldlon);
-	properties->lat=g_ascii_strtod (gtk_entry_get_text(GTK_ENTRY(latent)),NULL);
-	properties->lon=g_ascii_strtod (gtk_entry_get_text(GTK_ENTRY(lonent)), NULL);
-	properties->range=g_ascii_strtod (gtk_entry_get_text(GTK_ENTRY(rangeent)), NULL); 
-	printf("We are: %f%f\n",properties->lat,properties->lon);  
-	//Check Latitude/Longitude entries are correct
-	if(properties->lat > 89.9 || properties->lat < -89.9) {
-	//printf("Invalid Lat\n");
-	double homelat = oldlat; 
-	//printf("New Lat:%f\n", homelat);
-	gtk_entry_set_text(latent, g_strdup_printf("%f",properties->lat));
-	}
-	if(properties->lon > 180 || properties->lon < -180) {
-	//printf("Invalid Lon\n");
-	double homelon = oldlon; 
-	//printf("New Lon:%f\n", homelon);
-	gtk_entry_set_text(lonent, g_strdup_printf("%f",properties->lon));
-	}
-	//centre map on new coordinates after widget closed
-	osm_gps_map_set_center_and_zoom(map,properties->lat, properties->lon, 5);
-	aprsis_set_filter(properties->ctx, properties->lat,properties->lon,properties->range);
-	gtk_widget_hide(	GTK_WIDGET( popup ) );
-	return FALSE;
-}
-G_MODULE_EXPORT gboolean
-on_properties_hide_event (GtkWidget *widget, gpointer user_data)
-{
-	gtk_widget_hide(	GTK_WIDGET( popup ) );
-	return FALSE;
-}
-
-G_MODULE_EXPORT void
-on_close (GtkWidget *widget, gpointer user_data)
-{
-    gtk_widget_destroy(widget);
-    gtk_main_quit();
-}
 
 
 static void
