2c4ce3e64c5ece21b4f513d89084abc98a8f4538
Dave Hibberd <dave.hibberd@gmail.com>
2011-08-14 21:19:56 -0400

    we now have write_to_db instead of the mess of code that was in it's place. Looking at the change home issue, it's possible that in the pre-db write sprintf(), -ve float/doubles are not being written to string, hence them passing through the if(){} check. Will continue checking Hx

--- station.c
+++ station.c
@@ -273,26 +273,15 @@ static void position_station(APRSMapStation *station, fap_packet_t *packet) {
 			station->course = gjcp_direction(station->lon, station->lat, *(packet->longitude), *(packet->latitude));
 			station->lat = *(packet->latitude);
 			station->lon = *(packet->longitude);
-	printf("testicule");
-	if (station->lat && station->lon && packet->src_callsign) {
-	char zlat[10]; char zlon[10]; char zcourse[10];
- 	int n, m, o,rc;
-	n=sprintf(zlat,"%f",station->lat);   
-	m=sprintf(zlon,"%f",station->lon);
-	o=sprintf(zcourse, "%f", station->course);
 
-	char *zSQL = sqlite3_mprintf("INSERT INTO call_data (call, object, course, lon, lat) VALUES (%Q,%Q,%Q,%Q,%Q)",packet->src_callsign,packet->object_or_item_name,zcourse,zlon,zlat);
-	rc = sqlite3_exec(db, zSQL, 0, 0, &zErrMsg);
-	if( rc!=SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-    }
-	sqlite3_free(zSQL);
+	if (station->lat && station->lon && packet->src_callsign) {
+		write_to_db(station->lat, station->lon, station->course, packet->src_callsign, packet->object_or_item_name);
+
 	printf("NotFail\n"); }
 else {
 	printf("Fail\n");
 }
-/*write_to_db(station->lat, station->lon, station->course, packet->src_callsign, packet->object_or_item_name);*/
+
 			// we may need to create a track, then
 			if (!station->track) {
 				station->track = osm_gps_map_track_new();
@@ -332,18 +321,8 @@ else {
 	if (station->lat && station->lon && packet->src_callsign) {
 	printf("pass\n");
 	printf("%f\n,%f\n,%s\n",station->lat, station->lon, packet->src_callsign);
-	char zlat[10]; char zlon[10]; char zcourse[10];
- 	int n, m, o,rc;
-	n=sprintf(zlat,"%f",station->lat);   
-	m=sprintf(zlon,"%f",station->lon);
-	o=sprintf(zcourse, "%f", station->course);
-	char *zSQL = sqlite3_mprintf("INSERT INTO call_data (call, object, course, lon, lat) VALUES (%Q,%Q,%Q,%Q,%Q)",packet->src_callsign,packet->object_or_item_name,zcourse,zlon,zlat);
-	rc = sqlite3_exec(db, zSQL, 0, 0, &zErrMsg);
-	if( rc!=SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-    }
-	sqlite3_free(zSQL);
+	write_to_db(station->lat, station->lon, station->course, packet->src_callsign, packet->object_or_item_name);
+
 } else {
 printf("fail");
 }
@@ -381,12 +360,16 @@ gboolean process_packet(gchar *msg) {
 	}
 	g_hash_table_replace(stations, station->callsign, station);
 }
-/*void write_to_db(float latitude, float longitude, float course, char call, char object) {
+void write_to_db(gdouble latitude, gdouble longitude, float course, char *call, char *object) {
+
 	char zlat[10]; char zlon[10]; char zcourse[10];
  	int n, m, o,rc;
 	n=sprintf(zlat,"%f",latitude);   
 	m=sprintf(zlon,"%f",longitude);
 	o=sprintf(zcourse, "%f", course);
+
+printf("%s\n,%s\n,%s\n",zlat, zlon, call);
+
 	char *zSQL = sqlite3_mprintf("INSERT INTO call_data (call, object, course, lon, lat) VALUES (%Q,%Q,%Q,%Q,%Q)",call,object,zcourse,zlon,zlat);
 	rc = sqlite3_exec(db, zSQL, 0, 0, &zErrMsg);
 	if( rc!=SQLITE_OK ){
@@ -394,5 +377,5 @@ gboolean process_packet(gchar *msg) {
       sqlite3_free(zErrMsg);
     }
 	sqlite3_free(zSQL);
-}*/
+}
 /* vim: set noexpandtab ai ts=4 sw=4 tw=4: */
